4611fd48db1a89b451fc16145bfd0e6fe97fe5d7
jvrocha15 <jvar15122004@gmail.com>
2026-08-31 10:20:52 -0300

    fix: merge_newbranch

--- exercicio.js
+++ exercicio.js
@@ -1,6 +1,9 @@
+function somaDoisNumeros(a, b) {
+  let result = a + b;
 function divideDoisNumeros(a, b) {
   let result = a / b;
   console.log(result);
 }
 
+somaDoisNumeros(2,4)
 divideDoisNumeros(4, 2)
